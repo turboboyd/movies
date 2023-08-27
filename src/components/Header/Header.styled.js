36be5982a9f99f7c65752bb848447f9bebb0f8b5
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-08-27 12:14:47 +0200

    add

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -9,13 +9,23 @@ export const StyledLink = styled(NavLink)`
   text-transform: uppercase;
   border-radius: 14px;
   border: 1px solid #f6f6f6;
+  transition: color 0.5s ease, border 0.5s ease;
 
-  &:hover {
+  &:hover,
+  &:focus {
+    color: #cf0000;
     border: 1px solid #cf0000;
   }
 
   &.active {
     background-color: #cf0000;
-    border: none;
+    color: #ffffff;
+    border: 1px solid transparent;
+    transition: background-color 0.5s ease, color 0.5s ease, border 0.5s ease;
+    &:hover,
+    &:focus {
+      color: #ffffff;
+      border: 1px solid #ffffff;
+    }
   }
 `;
--- src/components/Poster/Poster.jsx
+++ src/components/Poster/Poster.jsx
@@ -2,16 +2,11 @@ import css from './Poster.module.css';
 import PropTypes from 'prop-types';
 
 export default function Poster({ imageUrl, altText, homepageLink }) {
-   if (!homepageLink) {
-     return (
-       <img
-         className={css.poster}
-         src={imageUrl}
-         alt={altText}
-         loading="lazy"
-       />
-     );
-   }
+  if (!homepageLink) {
+    return (
+      <img className={css.poster} src={imageUrl} alt={altText} loading="lazy" />
+    );
+  }
   return (
     <a href={homepageLink} target="_blank" rel="noopener noreferrer">
       <img className={css.poster} src={imageUrl} alt={altText} loading="lazy" />
